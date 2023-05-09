13b83435bb385865148d65e724d171adfe71dca5
Tech-Kat <katpelle@pursuit.org>
2023-05-09 19:12:00 -0400

    logo

--- capstone-frontend/src/App.js
+++ capstone-frontend/src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
